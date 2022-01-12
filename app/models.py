f945fc1b58b495f8b2151ecb9a10cd004c1b150b
Kamyyyk <kamyk962@gmail.com>
2022-01-12 18:58:42 +0100

    Dodano do .models 2 tabele Dodano do settings aplikacje ktora tworzy tabele Dodano do urls sciezke ktora daje dostep do funkcji hello Dodano funkcje hello

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Food(models.Model):
+    description = models.CharField(max_length=30)
+    calories = models.IntegerField()
+    created = models.DateTimeField()
+
+
+class Exercises(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now=True)
+    burned_calories = models.IntegerField
+
+
+
+
