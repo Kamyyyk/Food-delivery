1e774152034c254545a1c2c99d578b01c1f7fdcd
Kamyyyk <kamyk962@gmail.com>
2022-01-14 09:10:11 +0100

    x

--- mysite/models.py
+++ mysite/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Food(models.Model):
     description = models.CharField(max_length=30)
-    calories = models.IntegerField()
+    calories = models.IntegerField(default=1)
     created = models.DateTimeField()
 
     def __str__(self):
@@ -14,7 +14,7 @@ class Exercises(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now=True)
-    burned_calories = models.IntegerField()
+    burned_calories = models.IntegerField(default=0)
 
     def __str__(self):
         return self.description
