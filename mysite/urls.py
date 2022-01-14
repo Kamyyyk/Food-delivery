185304c8a533f9525dfd6c5f232f8259f3ee3c56
Kamyyyk <kamyk962@gmail.com>
2022-01-14 10:52:32 +0100

    poprawki

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('test/', views.test, name="test"),
 ]
 
--- mysite/views.py
+++ mysite/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 def index(request):
     name = "Hello"
     return HttpResponse(name)
+
+
+def test(request):
+    a = 10
+    b = 10
+    return HttpResponse(f"10 * 10 is {a*b}")
