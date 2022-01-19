f7c33fe33c62ac22c1d39829b56d98b71d9d5e33
Kamyyyk <kamyk962@gmail.com>
2022-01-19 10:11:30 +0100

    commit with ssh key included

--- mysite/models.py
+++ mysite/models.py
@@ -7,7 +7,7 @@ class Restaurant(models.Model):
     category = models.CharField(max_length=15)
 
     def __str__(self):
-        return self.name, self.name
+        return self.name
 
 
 class Votes(models.Model):
