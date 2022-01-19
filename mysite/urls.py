fc9034ed4fd87c758b3f60bed8e777df2d06d610
Kamyyyk <kamyk962@gmail.com>
2022-01-19 15:43:18 +0100

    Added 2 html sites which conect with database and gets elements from tables changed views.py now there is backend function for Votes and Restaurants Added to urls path for .html indexes

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('detail/<int:restaurant_id>', views.detail, name='detail'),
+    path('votes/', views.detail, name='detail'),
+    # path('detail/<int:restaurant_id>', views.detail, name='detail'),
     path('voteid/<int:restaurant_id>/', views.vote_desc, name='voteid'),
     path('vote/<int:restaurant_id>/', views.vote, name='vote'),
 
--- mysite/views.py
+++ mysite/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Votes, Restaurant
+from django.template import loader
 
 
 def index(request):
-    name = "Hello"
-    return HttpResponse(name)
+    vote_list = Votes.objects.order_by('-pub_date')[1:]
+    template = loader.get_template('mysite/index.html')
+    context = {
+        'vote_list': vote_list
+    }
+    return HttpResponse(template.render(context, request))
 
 
-def detail(requests, restaurant_id):
-    return HttpResponse(f'Restaurant nr: {restaurant_id}')
+def detail(requests):
+    restaurant_list = Restaurant.objects.order_by('-name')
+    template = loader.get_template('mysite/restaurants.html')
+    context = {
+        'restaurant_list': restaurant_list
+    }
+    return HttpResponse(template.render(context, requests))
 
 
 def vote_desc(requests, restaurant_id):
@@ -17,5 +28,4 @@ def vote_desc(requests, restaurant_id):
 
 def vote(requests, restaurant_id):
     return HttpResponse(f"You made a vote for restaurant {restaurant_id} ")
-
 
