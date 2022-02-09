0635f05faf6e83b416eda8e69d575e703d6e8e82
Kamyyyk <kamyk962@gmail.com>
2022-02-09 12:26:51 +0100

    added new empty functions to views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,48 +1,41 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Votes, Restaurant
+from django.template import loader
 from django.http import Http404
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(requests):
     return render(requests, 'mysite/not_logged.html')
 
 
-def loggout(request):
-    logout(request)
+def index(request):
+    return render(request, 'mysite/home.html')
+
+
+def logout(request):
     return render(request, 'mysite/not_logged.html')
 
 
-def restaurant_detail(requests):
-    try:
-        restaurants = Restaurant.objects.order_by('-name')
-    except Votes.DoesNotExist:
-        raise Http404("Nie ma restauracji o takim id")
-    return render(requests, "mysite/restaurants.html", {'restaurants': restaurants})
+def show_restaurants(requests):
+    restaurants = Restaurant.objects.order_by('-name')
+    context = {'restaurants': restaurants}
+    return render(requests, 'mysite/restaurants.html', context)
 
 
-def show_restaurant(requests, restaurant_id):
-    restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+def show_restaurant(requests, rid):
+    restaurant = get_object_or_404(Restaurant, pk=rid)
     context = {'restaurant': restaurant}
     return render(requests, "mysite/test.html", context)
 
 
-def my_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect('mysite:index')
-    else:
-        return redirect('mysite:login')
-
-
+def login(request):
+    return render(request, 'mysite/login.html')
 
 
 def register(request):
-    pass
+    return render(request, 'mysite/register.html')
 
 
 
