7327630e2534147284e26b1f5e544d8c6f3f5f6f
Kamyyyk <kamyk962@gmail.com>
2022-02-16 15:34:14 +0100

    changed urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path('restaurant/<int:rid>', views.show_restaurant, name='restaurant'),
     path('restaurants', views.show_restaurants, name='restaurants'),
     path('vote/<int:id>', views.vote, name='vote'),
-    path('addrestaurant', views.add_restaurant, name='add'),
     path('result', views.votes, name='result'),
+    path('restaurant/add', views.add_restaurant, name='add'),
+    path('restaurant/edit/<int:id>', views.edit_restaurant, name='edit_restaurant'),
+    path('restaurant/delete/<int:id>', views.delete_restaurant, name="delete_restaurant"),
 
     path('register', views.register_request, name='register'),
 ]
+
 
