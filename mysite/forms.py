9bb1dd6232e99126f09063cffd8660f2e71fe762
Kamyyyk <kamyk962@gmail.com>
2022-02-17 10:57:23 +0100

    changed forms.py

--- mysite/forms.py
+++ mysite/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import password_validation
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -6,19 +7,50 @@ from .models import *
 
 
 class NewUserForm(UserCreationForm):
+    password1 = forms.CharField(
+        label='Haslo',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+    )
+    password2 = forms.CharField(
+        label='Potwierdz haslo',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2'
+        ]
+        labels = {
+            'username': 'Nazwa uzytkownika',
+            'email': 'E-mail',
+            'password1': 'Haslo',
+            'password2': 'Powtorz haslo',
+        }
 
 
-class RestaurantForm(ModelForm):
+class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
-        fields = '__all__'
+        fields = field = [
+            'name',
+            'place',
+            'category'
+        ]
+        labels = {
+            'name': 'Nazwa',
+            'place': 'Miejsce',
+            'category': 'Kategoria',
+
+        }
 
 
-class VoteForm(ModelForm):
+class VoteForm(forms.ModelForm):
     class Meta:
         model = Votes
         fields = '__all__'
